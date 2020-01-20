8b678498f3e7ceaf954fb168d29cac75b832f009
savyerko <arminzone15@gmail.com>
2020-01-20 21:47:38 +0700

    Update script.js

--- proj/script.js
+++ proj/script.js
@@ -23,7 +23,7 @@ function renderPlaces(places) {
         let longitude = place.location.lng;
 
         let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${55.992706}; longitude: ${92.934862};`);
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', 'scene.gltf');
         model.setAttribute('rotation', '0 0 0');
         model.setAttribute('scale', '0.5 0.5 0.5');
