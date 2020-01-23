1d97267587936a3545f96af62276716f3c85e80c
savyerko <arminzone15@gmail.com>
2020-01-23 11:11:33 +0700

    Create script.js

---
+++ proj/script.js
@@ -0,0 +1,37 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Magnemite',
+            location: {
+                lat: 56.004015,
+                lng: 92.769762,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', 'scene.gltf');
+        model.setAttribute('rotation', '0 0 0');
+        model.setAttribute('scale', '2 2 2');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
